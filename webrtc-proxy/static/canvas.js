d1f6c435f677414da2b9ac4c590b6de9266694d0
Kevin Hansen <kevin.lhansen@hotmail.com>
2021-04-28 00:40:19 +0200

    Implement primitive 2D canvas functionality Avatars are hardcoded in but are draggable with the mouse The avatar X and Y positions are stored on the client (to be synced with server)

---
+++ webrtc-proxy/static/canvas.js
@@ -0,0 +1,125 @@
+var canvas = document.querySelector("canvas");
+var canvasBounding = canvas.getBoundingClientRect();
+var context = canvas.getContext("2d");
+
+var offsetX = canvasBounding.left;
+var offsetY = canvasBounding.top;
+
+// Dragging
+var dragging = false;
+var startX;
+var startY;
+
+// Avatars
+var avatars = [];
+avatars.push({ x: 25, y: 75, width: 15, height: 15, fill: "#444444", isDragging: false });
+avatars.push({ x: 50, y: 75, width: 15, height: 15, fill: "#ff0000", isDragging: false });
+avatars.push({ x: 75, y: 75, width: 15, height: 15, fill: "#0800ff", isDragging: false });
+
+// Render-loop
+function updateCanvasArea() {
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw rects in list
+    avatars.forEach((avatar) => {
+        context.fillStyle = avatar.fill;
+        rect(avatar.x, avatar.y, avatar.width, avatar.height);
+    });
+
+    requestAnimationFrame(updateCanvasArea);
+}
+
+// Mouse events
+canvas.onmousedown = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Get mouse position
+    var mouseX = parseInt(event.clientX - offsetX);
+    var mouseY = parseInt(event.clientY - offsetY);
+
+    // Find mouse-avatar overlap
+    avatars.forEach((avatar) => {
+        if (mouseX > avatar.x && mouseX < avatar.x + avatar.width && mouseY > avatar.y && mouseY < avatar.y + avatar.height) {
+            dragging = true;
+            avatar.isDragging = true;
+        }
+    });
+    // Save start of drag mouse position
+    startX = mouseX;
+    startY = mouseY;
+}
+
+canvas.onmouseup = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Reset all dragging-related variables
+    dragging = false;
+    avatars.forEach((avatar) => {
+        avatar.isDragging = false;
+    });
+}
+
+canvas.onmousemove = (event) => {
+    if (dragging) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // Get mouse position
+        var mouseX = parseInt(event.clientX - offsetX);
+        var mouseY = parseInt(event.clientY - offsetY);
+
+        // Calculate distance moved since last mousemove event
+        var deltaX = mouseX - startX;
+        var deltaY = mouseY - startY;
+
+        // Move avatars marked as dragging (isDraggin = true)
+        avatars.forEach((avatar) => {
+            if (avatar.isDragging) {
+                avatar.x += deltaX;
+                avatar.y += deltaY;
+            }
+        });
+
+        // Update start variables for next mousemove event
+        startX = mouseX;
+        startY = mouseY;
+    }
+}
+
+// WASD controls
+var moveSpeed = 10;
+
+window.onkeydown = (event) => {
+
+    if (["ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight"].includes(event.key)) {
+        event.preventDefault();
+    }
+
+    switch (event.key) {
+        case "ArrowUp":
+            avatar.y -= moveSpeed;
+            break;
+        case "ArrowLeft":
+            avatar.x -= moveSpeed;
+            break;
+        case "ArrowDown":
+            avatar.y += moveSpeed;
+            break;
+        case "ArrowRight":
+            avatar.x += moveSpeed;
+            break;
+    }
+}
+
+function rect(x, y, width, height) {
+    context.beginPath();
+    context.rect(x, y, width, height);
+    context.closePath();
+    context.fill();
+}
+
+// Trigger render-loop
+requestAnimationFrame(updateCanvasArea);
