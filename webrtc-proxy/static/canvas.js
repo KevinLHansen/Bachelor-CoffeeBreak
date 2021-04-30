e2fadc22846105342a5d2e18e325bc32d01d52a5
Kevin Hansen <kevin.lhansen@hotmail.com>
2021-04-30 22:36:01 +0200

    Change avatar functionality to act dynamically with users logging in - An avatar with random color and position gets added when a user logs in - Avatars are automatically removed upon disconnect from corresponding user

--- webrtc-proxy/static/canvas.js
+++ webrtc-proxy/static/canvas.js
@@ -12,9 +12,6 @@ var startY;
 
 // Avatars
 var avatars = [];
-// avatars.push({ x: 25, y: 75, width: 15, height: 15, fill: "#444444", isDragging: false });
-// avatars.push({ x: 50, y: 75, width: 15, height: 15, fill: "#ff0000", isDragging: false });
-// avatars.push({ x: 75, y: 75, width: 15, height: 15, fill: "#0800ff", isDragging: false });
 
 // Render-loop
 function updateCanvasArea() {
