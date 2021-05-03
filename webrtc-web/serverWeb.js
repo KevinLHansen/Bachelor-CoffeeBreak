dc771ff50dc85d2d1524e9a768b2e923295ab255
Benjamin Klerens <bekle18@student.sdu.dk>
2021-05-03 23:30:50 +0200

    Added liveness probe for webserver, updated webserver to log as well

--- webrtc-web/serverWeb.js
+++ webrtc-web/serverWeb.js
@@ -2,10 +2,12 @@ const https = require('https');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const requestIp = require('request-ip');
 
 const port = 8080;
 
 var server = express();
+server.use(requestIp.mw())
 
 // Certificate config
 const conf = {
@@ -13,11 +15,15 @@ const conf = {
     cert: fs.readFileSync('cert/cert.pem')
 };
 
+
 // Static resources
 server.use(express.static('static'));
 
+
 server.get('/', (req, res) => {
+    const ip = req.clientIp
     res.sendFile(path.join(__dirname + "/index.html"));
+    console.log("User from " + ip + " requested root");
 });
 
 https.createServer(conf, server).listen(port, () => {
