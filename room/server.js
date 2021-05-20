3225da95a3de53c622f5d79c48856ab4f7183c1b
Kevin Hansen <kevin.lhansen@hotmail.com>
2021-05-20 16:13:36 +0200

    Implement old frontend and application system to new cloud structure Slight issues with WebRTC: can talk with self, not others (???)

--- room/server.js
+++ room/server.js
@@ -13,6 +13,13 @@ var httpServer = express();
 httpServer.use(requestIp.mw());
 httpServer.use(express.static('static'));
 
+var users = {}; // Key-value pairs of username:connection
+var room = {
+    owner: "",
+    users: [],
+    avatars: []
+};
+
 // WEB SERVER
 
 http.createServer(httpServer).listen(http_port, () => {
@@ -46,17 +53,187 @@ wsServer.on('connection', (connection) => {
 
         switch (data.type) {
 
-            default: connection.send("Unknown command");
-            break;
+            case "login":
+                logs("[login]: " + message);
+
+                if (users[data.name]) { // Check if user already exists / is logged
+                    sendTo(connection, {
+                        type: "login",
+                        success: false
+                    });
+                } else { // Register user
+                    users[data.name] = connection;
+                    connection.name = data.name;
+                    room.users.push(data.name);
+
+                    sendTo(connection, {
+                        type: "login",
+                        room: room,
+                        success: true
+                    });
+                    logs("User logged: " + data.name);
+
+                    createAvatar(data);
+                    sendRoomUpdate();
+                }
+                break;
+
+            case "chat":
+                logs("[chat]: " + message);
+                // Relay chat message to all users
+                room.users.forEach((user) => {
+                    sendTo(users[user], data);
+                });
+                break;
+
+            case "canvasUpdate":
+                log("Canvas update received from: " + data.name);
+                // Update room avatars on server
+                room.avatars = data.avatars;
+                // Relay canvas update to all users
+                room.users.forEach((user) => {
+                    sendTo(users[user], data);
+                });
+                break;
+
+            case "offer":
+                log("[offer]: " + data.offerer + " -> " + data.answerer);
+                // Relay offer to answerer
+                sendTo(users[data.answerer], data);
+                break;
+
+            case "answer":
+                log("[answer]: " + data.offerer + " <- " + data.answerer);
+                // Relay answer to offerer
+                sendTo(users[data.offerer], data);
+                break;
+
+            case "candidate":
+                log("[candidate]: " + data.name);
+                // Relay candidate to all users except self
+                room.users.forEach((user) => {
+                    if (user != data.name) {
+                        sendTo(users[user], data);
+                    }
+                });
+                break;
+
+            default:
+                logs("[error]: " + data.type + " not recognized");
+                sendTo(connection, {
+                    type: "error",
+                    message: "Command not found: " + data.type
+                });
         }
     });
 
     connection.on('close', () => {
-        logs("Connection closed");
+        if (connection.name) {
+            logs("Connection closed: " + connection.name);
+
+            leaveRoom(connection.name);
+            removeAvatar(connection.name);
+
+            delete users[connection.name]
+        }
     });
 });
 
+// Avatar functions
+
+function createAvatar(data) {
+    var size = 25;
+
+    // Get canvas measurements from data
+    var canvasWidth = data.canvas.width;
+    var canvasHeight = data.canvas.height;
+
+    var horiMargin = Math.floor(canvasWidth / 3);
+    var vertMargin = Math.floor(canvasHeight / 3);
+    // Get random position for avatar
+    var x = Math.floor(Math.random() * (canvasWidth - horiMargin * 2) + horiMargin);
+    var y = Math.floor(Math.random() * (canvasHeight - vertMargin * 2) + vertMargin);
+    // Get random hex color
+    var color = Math.floor(Math.random() * 16777215).toString(16);
+    // Sometimes results in strings that are one digit too short (5 instead of 6)
+    if (color.length == 5) {
+        color = "0" + color;
+    }
+
+    room.avatars.push({ name: data.name, x: x, y: y, width: size, height: size, fill: `#${color}`, isDragging: false });
+
+    sendCanvasUpdate();
+}
+
+function removeAvatar(username) {
+    // Get list of avatars of room associated with user
+    var avatars = room.avatars;
+    for (let i = 0; i < avatars.length; i++) {
+        if (avatars[i].name === username) {
+            avatars.splice(i, 1);
+        }
+    }
+    sendCanvasUpdate();
+}
+
+function leaveRoom(username) {
+    // Remove username from room
+    for (let i = 0; i < room.users.length; i++) {
+        if (room.users[i] === username) {
+            room.users.splice(i, 1);
+        }
+    }
+    sendRoomUpdate(username);
+}
+
+function sendRoomUpdate(leaver) {
+
+    room.users.forEach((user) => {
+        // Get connection of user
+        var connection = users[user];
+        // Compose leave message
+        var msg = {
+            type: "roomUpdate",
+            room: room
+        };
+        if (leaver) {
+            msg["leaver"] = leaver;
+        }
+        sendTo(connection, msg);
+    });
+}
+
+// Sends a canvas update to all users
+function sendCanvasUpdate() {
+    for (user in users) {
+        sendTo(users[user], {
+            type: "canvasUpdate",
+            avatars: room.avatars,
+            name: "SERVER"
+        });
+    }
+}
+
+// Relays an offer to all users of a given room
+function relayOffer(offer, sender) {
+    // Relay offer to all users in room
+    users.forEach((user) => {
+        // Exclude sender
+        if (user !== sender) {
+            sendTo(users[user], {
+                type: "offer",
+                name: sender,
+                offer: offer
+            });
+        }
+    });
+}
+
 // Utility functions
+
+function sendTo(connection, message) {
+    connection.send(JSON.stringify(message));
+}
 
 function logw(data) {
     log("[W] " + data);
--- room/static/client.js
+++ room/static/client.js
@@ -4,20 +4,59 @@ const server_port = "8082";
 
 // Variables
 var webSocket;
+var username = ""; // Own username
+var room; // Current room
+var roomId; // Current room ID
+var localStream; // Own MediaStream
+var peerConnections = []; // List of peer connections
+
+// Config for dynamic volume levels
+var close = { threshold: 75, volume: 1 };
+var medium = { threshold: 200, volume: 0.1 };
+var far = { threshold: 400, volume: 0 };
 
 // UI elements
-var button = document.getElementById("button");
-var input = document.getElementById("input");
+
+var loginView = document.getElementById("loginView");
+var loginCard = document.getElementById("loginCard");
+var roomView = document.getElementById("roomView");
+
+var loginInput = document.getElementById("loginInput");
+var loginBtn = document.getElementById("loginBtn");
+
+var msgInput = document.getElementById("msgInput");
+var sendBtn = document.getElementById("sendBtn");
+var leaveRoomBtn = document.getElementById("leaveRoomBtn");
+
+var chatTxt = document.getElementById("chatTxt");
+
+var roomTxt = document.getElementById("roomTxt");
+var usersTxt = document.getElementById("usersTxt");
+
+var audioContainer = document.getElementById("audioContainer");
+
+// WebRTC UserMedia initiation
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
+    localStream = stream;
+}).catch((error) => {
+    log("getUserMedia error: " + error);
+});
 
 // WebSocket initiation
+var url = `wss://${server_ip}${room_path}ws`; // in-cluster
+//var url = `ws://${server_ip}:8082`; // locally
 
-var url = `${server_ip}${room_path}ws`;
 log(`Client connecting to WebSocket: ${url}`);
-webSocket = new WebSocket(`wss://${url}`);
+webSocket = new WebSocket(`${url}`);
 
 webSocket.onopen = () => {
     log("Client connected to WebSocket");
 };
+
+webSocket.onclose = () => {
+    log("WebSocket connection closed");
+}
 
 webSocket.onmessage = (message) => {
     // Parse message data from JSON
@@ -25,9 +64,24 @@ webSocket.onmessage = (message) => {
 
     switch (data.type) {
 
+        case "login": // Login attempts response
+            log("[login]: " + message.data);
+            if (data.success) {
+                log("User logged in: " + username);
+                // Update room variables
+                room = data.room;
+                // Update UI
+                updateUI("inroom");
+                updateUsersUI();
+                // Send WebRTC offers
+                sendOffers();
+            } else {
+                alert("Username taken");
+            }
+            break;
+
         case "roomUpdate":
             log("[roomUpdate]: " + message.data);
-
             room = data.room; // Update room state
 
             // Update UI
@@ -47,20 +101,19 @@ webSocket.onmessage = (message) => {
             }
             break;
 
-        case "chat": // Incoming chat
-            log("[chat]: " + message.data);
-
-            // Insert message in chatbox
-            //chatTxt.value = data.name + ": " + data.message + "\n" + chatTxt.value; // Most recent on top
-            chatTxt.value = chatTxt.value + data.name + ": " + data.message + "\n"; // Most recent on bottom
-            break;
-
         case "canvasUpdate":
             log("[canvasUpdate]: " + message.data);
             if (room) {
                 room.avatars = data.avatars;
                 updateVolumes();
             }
+            break;
+
+        case "chat":
+            log("[chat]: " + message.data);
+            // Insert message in chatbox
+            //chatTxt.value = data.name + ": " + data.message + "\n" + chatTxt.value; // Most recent on top
+            chatTxt.value = chatTxt.value + data.name + ": " + data.message + "\n"; // Most recent on bottom
             break;
 
         case "offer":
@@ -90,8 +143,253 @@ webSocket.onmessage = (message) => {
     }
 };
 
+// UI HANDLING
+
+// Login (JOIN) button
+loginBtn.addEventListener("click", (event) => {
+    if (loginInput.value) {
+        username = loginInput.value;
+
+        send({
+            type: "login",
+            canvas: { width: canvas.width, height: canvas.height }
+        });
+    }
+})
+
+// Leave room button
+leaveRoomBtn.addEventListener("click", (event) => {
+    window.location.pathname = "";
+});
+
+// Chat message input
+msgInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        if (msgInput.value) {
+            var msg = msgInput.value;
+            log("Sending chat message: " + msg);
+            send({
+                type: "chat",
+                message: msg
+            });
+            msgInput.value = "";
+        }
+    }
+});
+
+// Add keyup eventListeners to elements which need them ([input, button])
+[
+    [loginInput, loginBtn],
+
+].forEach((element) => {
+    element[0].addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            element[1].click();
+        }
+    });
+});
+
+// Sends offers to all other users in room
+function sendOffers() {
+    log("Sending offers");
+    room.users.forEach((user) => {
+        // Don't send to self
+        if (user !== username) {
+            var localConnection = createPeerConnection(user);
+            localConnection.createOffer().then((localDesc) => {
+                localConnection.setLocalDescription(localDesc);
+                send({
+                    type: "offer",
+                    offer: localDesc,
+                    offerer: username,
+                    answerer: user
+                });
+            });
+            // Add the peer connection to local list
+            peerConnections.push(localConnection);
+        }
+    });
+}
+
+// Handles incoming offer and sends answer
+function sendAnswer(offerer, offer) {
+    log("Sending answer to: " + offerer);
+    var localConnection = createPeerConnection(offerer);
+    localConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    // Create answer to offer
+    localConnection.createAnswer().then((localDesc) => {
+        localConnection.setLocalDescription(localDesc);
+        // Send answer to offerer
+        send({
+            type: "answer",
+            answer: localDesc,
+            offerer: offerer,
+            answerer: username
+        });
+    });
+    // Add the peer connection to local list
+    peerConnections.push(localConnection);
+}
+
+// Creates a RTCPeerConnection to given remote user
+function createPeerConnection(user) {
+    try {
+        var localConnection = new RTCPeerConnection();
+        // Set association name
+        localConnection.name = user;
+
+        // Set event handlers
+        localConnection.onicecandidate = (event) => {
+            log("ICE candidate got");
+
+            // Send the candidate
+            if (event.candidate) {
+                send({
+                    type: "candidate",
+                    candidate: event.candidate,
+                    name: username
+                });
+            }
+        };
+        localConnection.ontrack = (event) => {
+            log("Track got");
+
+            var stream = event.streams[0];
+            var audio = createAudioElement(user, stream);
+
+            audioContainer.appendChild(audio);
+            updateVolumes();
+        };
+
+        // Add local stream to the connection
+        localStream.getAudioTracks().forEach((track) => {
+            localConnection.addTrack(track, localStream);
+        });
+
+        return localConnection;
+    } catch (error) {
+        log("Error creating peer connection: " + error);
+    }
+}
+
+// Creates a DOM audio element for appendage to HTML page
+function createAudioElement(username, mediaStream) {
+    var audioElement = document.createElement("audio");
+    audioElement.id = username;
+    audioElement.srcObject = mediaStream;
+    //audioElement.setAttribute("controls", true);
+    audioElement.setAttribute("autoplay", true)
+
+    return audioElement;
+}
+
+// Disables/enables multiple HTML elements at a time
+function setDisabled(elements, disabled) {
+    elements.forEach((element) => {
+        element.disabled = disabled;
+    });
+}
+
+// Updates client UI based on given state
+function updateUI(state) {
+    switch (state) {
+        case "inroom":
+            loginCard.style.display = "none";
+            loginView.style.display = "none";
+            roomView.style.display = "flex";
+            roomTxt.innerHTML = window.location.pathname;
+            break;
+    }
+}
+
+// Updates UI element which lists users in current room
+function updateUsersUI() {
+    var newText = "";
+    for (i = 0; i < room.users.length; i++) {
+        newText += room.users[i];
+        if (!(i == room.users.length - 1)) { // No comma on last
+            newText += ", ";
+        }
+    }
+    usersTxt.innerHTML = newText;
+}
+
+// Updates volumes of all audio elements according to avatar distance
+function updateVolumes() {
+    if (room) {
+        // Get own avatar
+        ownAvatar = getAvatar(username);
+
+        // Get all audio elements in audio container
+        var audioElements = audioContainer.getElementsByTagName("audio");
+        for (i = 0; i < audioElements.length; i++) {
+            var audioElement = audioElements[i];
+
+            // Get id (user association) of element
+            var id = audioElement.getAttribute('id');
+
+            // Get avatar of associated user
+            var avatar = getAvatar(id);
+
+            // Get distance between avatars
+            var distance = getDistance(ownAvatar, avatar);
+            log("DIST: " + username + " <-> " + id + " = " + distance);
+
+            // Adjust volume of audio element
+            var volume;
+            if (distance >= far.threshold) { // "far" away
+                volume = far.volume;
+            } else if (distance >= medium.threshold) { // "medium" away
+                volume = medium.volume;
+            } else if (distance <= close.threshold) { // "close"
+                volume = close.volume;
+            } else { // in between
+                volume = convertRange(distance, {
+                    min: close.threshold,
+                    max: medium.threshold
+                }, {
+                    min: close.volume,
+                    max: medium.volume
+                });
+            }
+            audioElement.volume = volume;
+            //log("VOLUME: " + volume);
+        }
+    }
+}
+
+// Returns the length of the direct vector between two avatars
+function getDistance(avatar1, avatar2) {
+    var vector = {
+        x: avatar1.x - avatar2.x,
+        y: avatar1.y - avatar2.y
+    };
+    // Pythagoras
+    var distance = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
+    return distance;
+}
+
+// Returns value convert from one range to another
+function convertRange(value, oldRange, newRange) {
+    return ((value - oldRange.min) * (newRange.max - newRange.min)) / (oldRange.max - oldRange.min) + newRange.min;
+}
+
+// Returns avatar object for given name in current room
+function getAvatar(name) {
+    var avatarGet;
+    room.avatars.forEach((avatar) => {
+        if (avatar.name == name) {
+            avatarGet = avatar;
+        }
+    });
+    return avatarGet;
+}
+
 // Sends data to WebSocket
 function send(message) {
+    message.name = username;
     webSocket.send(JSON.stringify(message));
 }
 
